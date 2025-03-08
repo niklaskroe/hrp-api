fea0c6c1333a1295e813d634fe0c35399b6acfd1
niklaskroe <kroegerniklas@gmx.de>
2025-03-08 13:51:06 +0100

    feat: extend controller boilerplate code

--- src/controllers/item.controller.js
+++ src/controllers/item.controller.js
@@ -1,16 +1,32 @@
 import ItemService from "../services/item.service.js";
 import express from "express";
-import mongoose from "mongoose";
 
 const itemController = express.Router();
 
 // Controller functions
 itemController.get("/items", async (req, res) => {
     res.send("GET /items");
+    await ItemService.test();
 });
 
 itemController.get("/items/:id", async (req, res) => {
     res.send("GET /items/" + req.params.id);
+});
+
+itemController.post("/items", async (req, res) => {
+    res.send("POST /items: " + JSON.stringify(req.body));
+});
+
+itemController.put("items/:id", async (req, res) => {
+    res.send("PUT /items/" + req.params.id + ": " + JSON.stringify(req.body));
+});
+
+itemController.patch("items/:id", async (req, res) => {
+    res.send("PATCH /items/" + req.params.id + ": " + JSON.stringify(req.body));
+});
+
+itemController.delete("items/:id", async (req, res) => {
+    res.send("DELETE /items/" + req.params.id);
 });
 
 export default itemController;
