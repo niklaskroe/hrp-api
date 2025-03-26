3b522d94f5c285896f88fe9452b8c816154d4444
niklaskroe <kroegerniklas@gmx.de>
2025-03-26 20:36:47 +0100

    feat: fix mqtt publisher usage

--- src/main.js
+++ src/main.js
@@ -6,20 +6,19 @@ import dotenv from "dotenv";
 import logging from "logging";
 import process from "node:process";
 import path from "node:path";
-import { fileURLToPath } from "node:url";
+import {fileURLToPath} from "node:url";
 import url from "node:url";
 
 import db from "./database/database.js";
-// import mqttService from "./mqtt/mqtt.service.js";
 import itemController from "./controllers/item.controller.js";
 import storageController from "./controllers/storage.controller.js";
 import shoppingListController from "./controllers/shoppingList.controller.js";
+import mqttPublisher from "./mqtt/mqtt.publisher.js";
 
 dotenv.config();
 
 const config = {
-    host: process.env.HOST || "localhost",
-    port: process.env.PORT || 8080,
+    host: process.env.HOST || "localhost", port: process.env.PORT || 8080,
 }
 
 const logger = logging.default("main");
@@ -36,7 +35,6 @@ app.use((req, res, next) => {
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-logger.info(__dirname + ' ' + __filename);
 
 const sourceDir = path.dirname(url.fileURLToPath(import.meta.url));
 const staticDir = path.join(sourceDir, "..", "static");
@@ -50,16 +48,19 @@ app.use(itemController, storageController, shoppingListController);
 
 db.connect().then(() => logger.info("Database connected."));
 
+mqttPublisher.connect();
+
 const server = app.listen(config.port, config.host, () => {
     logger.info(`Server listening on http://${config.host}:${config.port}`);
 });
 
 app.get("/status", async (req, res) => {
     const mongoState = mongoose.connection.readyState; // 1 = connected, 0 = disconnected
+    const mqttState = mqttPublisher.isConnected();
 
     return res.json({
-        status: mongoState === 1 ? "✅ Connected to MongoDB" : "❌ Not Connected",
-        mongoState
+        mongoStatus: mongoState === 1 ? "✅ Connected to MongoDB" : "❌ Not Connected",
+        mqttStatus: mqttState === true ? "✅ Connected to MQTT" : "❌ Not Connected"
     });
 });
 
